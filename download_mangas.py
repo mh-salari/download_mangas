4a2d52f49dfcb0fb99b718d49fab32755ca362a1
mh-salari <mh.salari@sutech.ac.ir>
2021-04-04 21:05:28 +0430

    first commit

---
+++ download_mangas.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 4 2021
+@author: MohammadHossein Salari
+@email: mohammad.hossein.salari@gmail.com
+"""
+
+import os
+import shutil
+from telethon import TelegramClient
+from config import *
+from download_utils import *
+
+base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+chapters_path = os.path.join(base_path, "chapters_url")
+session_path = os.path.join(base_path, "manga.session")
+
+client = TelegramClient(session_path, api_id, api_hash)
+
+
+@retry(delay=15, backoff=2, tries=5)
+def download_mangas(manga_rss_url):
+    manga_name = os.path.basename(manga_rss_url)[:-4]
+    manga_chapters_path = os.path.join(base_path, "chapters_url", f"{manga_name}.txt")
+    chapters_url_on_disk = []
+    if os.path.exists(manga_chapters_path):
+        print(f'Loadding list of "{manga_name}" chapters from disk')
+        with open(manga_chapters_path, "r") as f:
+            chapters_url_on_disk = [line.rstrip("\n") for line in f]
+
+    print(f'Getting list of "{manga_name}" chapters from mangasee123.com')
+    chapters_url = [chapter["link"] for chapter in get_list_of_chapters(manga_rss_url)]
+
+    if chapters_url_on_disk == []:
+        new_chapters = [chapters_url[0]]
+    else:
+        new_chapters = set(chapters_url) - set(chapters_url_on_disk)
+    if new_chapters:
+        for chapter_url in new_chapters:
+            chapter_url = chapter_url.replace("-page-1.html", "")
+
+            print(f"Gettin list of {os.path.basename(chapter_url)} images")
+            images_url = get_url_of_images(chapter_url)
+
+            print(f"Downloading {os.path.basename(chapter_url)} images")
+            url_and_images = download_images(images_url)
+
+            save_path = os.path.join(base_path, "temp", os.path.basename(chapter_url))
+            print(f"Saveing chapter to {save_path}")
+            save_manga(save_path, url_and_images, save_png=False, save_pdf=True)
+
+            print(f"Download {os.path.basename(chapter_url)} Finished!")
+    else:
+        print("Notting new to download!")
+
+    with open(manga_chapters_path, "w") as f:
+        [f.write(chapter + "\n") for chapter in chapters_url]
+    print("-" * 50)
+
+
+async def main():
+
+    mangas_rss_url_path = os.path.join(base_path, "mangas_rss_url.txt")
+    if not os.path.exists(mangas_rss_url_path):
+        raise Exception("mangas_rss_url.txt doesn't exist!")
+
+    with open(mangas_rss_url_path, "r") as f:
+        mangas_rss_url = [line.rstrip("\n") for line in f]
+
+    print("Check mangas for new chapter")
+    print("#" * 50)
+    for manga_rss_url in mangas_rss_url:
+        download_mangas(manga_rss_url)
+    print("#" * 50)
+    print("Downloading finished!")
+
+    mangas_pdf = []
+    for dirpath, dirnames, filenames in os.walk(temp_path):
+        for filename in [f for f in filenames if f.endswith(".pdf")]:
+            mangas_pdf.append(os.path.join(dirpath, filename))
+
+    if mangas_pdf:
+        print("Sending PDF to my telegram account")
+    for pdf in tqdm(mangas_pdf):
+
+        await client.send_message(
+            "mh_salari", f"{os.path.basename(pdf)[:-4].replace('-',' ')}"
+        )
+
+        await client.send_file("mh_salari", pdf)
+
+
+if __name__ == "__main__":
+
+    temp_path = os.path.join(base_path, "temp")
+
+    if not os.path.exists(temp_path):
+        os.makedirs(temp_path)
+
+    with client:
+        client.loop.run_until_complete(main())
+
+    shutil.rmtree(temp_path, ignore_errors=True)
+
---
+++ download_utils.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 4 2021
+@author: MohammadHossein Salari
+@email: mohammad.hossein.salari@gmail.com
+
+@sources:
+         - https://codeburst.io/building-an-rss-feed-scraper-with-python-73715ca06e1f
+         - https://stackoverflow.com/questions/7391945/how-do-i-read-image-data-from-a-url-in-python
+         - https://stackoverflow.com/questions/27327513/create-pdf-from-a-list-of-images
+"""
+import requests
+from bs4 import BeautifulSoup
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+import re
+import os
+from PIL import Image
+from tqdm import tqdm
+from retry import retry
+
+
+def get_list_of_chapters(manga_rss_url):
+
+    chapters_list = []
+
+    try:
+        r = requests.get(manga_rss_url)
+    except Exception as e:
+        print(e)
+
+    soup = BeautifulSoup(r.content, features="xml")
+
+    chapters = soup.findAll("item")
+
+    for a in chapters:
+        title = a.find("title").text
+        link = a.find("link").text
+        published_date = a.find("pubDate").text
+        chapter = {"title": title, "link": link, "published_date": published_date}
+        chapters_list.append(chapter)
+    return chapters_list
+
+
+def get_list_of_chapters(manga_rss_url):
+
+    chapters_list = []
+
+    try:
+        r = requests.get(manga_rss_url)
+    except Exception as e:
+        print(e)
+
+    soup = BeautifulSoup(r.content, features="xml")
+
+    chapters = soup.findAll("item")
+
+    for a in chapters:
+        title = a.find("title").text
+        link = a.find("link").text
+        published_date = a.find("pubDate").text
+        chapter = {"title": title, "link": link, "published_date": published_date}
+        chapters_list.append(chapter)
+    return chapters_list
+
+
+def get_url_of_images(chapter_url):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+
+    driver.get(chapter_url)
+
+    timeout = 60
+    for i in range(timeout):
+        if "{" in driver.title:
+            time.sleep(1)
+        else:
+            break
+    else:
+        raise Exception("timeout")
+
+    images_url = re.findall(
+        r"(?:http\:|https\:)?\/\/.*?manga.*?\.(?:png|jpg)", driver.page_source
+    )
+    driver.quit()
+
+    images_url = list(set(images_url))
+    images_url.sort()
+
+    return images_url
+
+
+def download_images(images_url):
+    images = []
+    for url in tqdm(images_url):
+        image = Image.open(requests.get(url, stream=True).raw)
+        images.append([url, image])
+    return images
+
+
+def save_manga(save_path, url_and_images, save_png=True, save_pdf=True):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    if save_png:
+        for url, image in url_and_images:
+            image.save(os.path.join(save_path, os.path.basename(url)))
+    if save_pdf:
+        images = [image for _, image in url_and_images]
+        images[0].save(
+            os.path.join(save_path, f"{os.path.basename(save_path)}.pdf"),
+            "PDF",
+            resolution=100.0,
+            save_all=True,
+            append_images=images[1:],
+        )
